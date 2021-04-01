5c44e3469d5c987270ab5c441ef45da31ebfc249
Galahad3x <joelaumedes@gmail.com>
2021-04-01 15:46:54 +0200

    client proves

--- discordbot.py
+++ discordbot.py
@@ -5,11 +5,40 @@ from dotenv import load_dotenv
 
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
+GUILD = os.getenv('DISCORD_SERVER')
 
 client = discord.Client()
 
+
 @client.event
 async def on_ready():
-    print(f'{client.user} has connected to Discord!')
+	print(f'{client.user} has connected to Discord!')
+
+
+@client.event
+async def on_member_join(member):
+	await member.create_dm()
+	await member.dm_channel.send(f'Hi {member.name}, welcome to my Discord server!')
+
+
+@client.event
+async def on_message(message):
+	if message.author == client.user:
+		return
+
+	if message.content == '99!':
+		await message.channel.send(":100:")
+	elif message.content == 'raise-exception':
+		raise discord.DiscordException
+
+
+@client.event
+async def on_error(event, *args, **kwargs):
+	with open('err.log', 'a') as f:
+		if event == 'on_message':
+			f.write(f'Unhandled message: {args[0]}\n')
+		else:
+			raise
+
 
 client.run(TOKEN)
